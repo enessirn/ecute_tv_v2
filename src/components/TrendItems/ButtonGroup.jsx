c9833e2d9fa62785ef2e4b313c5e7a65d4ad6f33
Enes SIRIN <enessirinbusiness@gmail.com>
2024-12-19 12:12:57 +0300

    button didn't click fixed

--- src/components/TrendItems/ButtonGroup.jsx
+++ src/components/TrendItems/ButtonGroup.jsx
@@ -68,7 +68,7 @@ function ButtonGroup() {
             d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
           />
         </svg>
-        <span className="hidden md:!block">Add to favorites</span>
+        <span className="hidden md:!block pointer-events-none">Add to favorites</span>
       </button>
       <button
         className="flex flex-row items-center text-sm bg-[#c8143b] px-6 py-3 rounded-xl gap-1 hover:shadow-2xl hover:bg-[#e43232]"
@@ -95,7 +95,7 @@ function ButtonGroup() {
             d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
           />
         </svg>
-        See the details
+        <span className="pointer-events-none">See the details</span>
       </button>
     </div>
   );
